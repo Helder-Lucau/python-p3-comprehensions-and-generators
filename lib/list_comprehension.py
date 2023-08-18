b8ccde10132ecd8390cd18b4356c42c954da0de5
Helder <helderlucau@gmail.com>
2023-08-18 21:46:31 +0300

    Completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
+
 def return_evens(num_list):
-    pass
+    list = [i for i in num_list if i % 2 == 0]
+
+    return list
+
 
 def make_exclamation(sentence_list):
-    pass
+    list_of_sentences = [sentences + "!" for sentences in sentence_list]
+
+    return list_of_sentences
+
